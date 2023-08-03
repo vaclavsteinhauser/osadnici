caa7f1a90070f4e0426e96eaa634df7ec65fad5a
Václav Steinhauser <vaclav.steinhauser@gmail.com>
2023-08-04 00:18:23 +0200

    verzovani testovaciho projektu

---
+++ test-docker/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using databaze;
+using Microsoft.EntityFrameworkCore;
+
+using (var db = new GameContext())
+{
+    var hra = db.hry.Include("hraci").ToList()
+        ;
+    
+    /*db.Database.EnsureCreated();
+    Hrac v = new Hrac
+    {
+        Name = "Vaclav"
+    };
+    Hrac j = new Hrac
+    {
+        Name = "Janka"
+    };
+    Hra h = new Hra();
+    Mapka m = new Mapka();
+    db.hraci.Add(v);
+    db.hraci.Add(j);
+    db.hry.Add(h);
+    db.mapky.Add(m);
+    h.hraci = new List<Hrac>();
+    h.mapka = new Mapka();
+    h.hraci.Add(v);
+    h.hraci.Add(j);
+    db.SaveChanges();
+ */
+}
